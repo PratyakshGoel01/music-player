1e40c7cbb7a5481dc25d1fe3f8ba7d547e2a645d
Harsh Talwar <htalwar2308@gmail.com>
2022-09-16 23:12:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="left-content">
+      </div>
+      <div className="right-content">
+        <navbar className="navbar"></navbar>
+        <div className="content"></div>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
